4732b78543db8b40f4e6388b21dee094b1951ab2
Alexander <Alexander.Emanuelsson94@gmail.com>
2019-09-14 22:40:04 +0200

    Clear up the app.py file

--- rustdavinci/app.py
+++ rustdavinci/app.py
@@ -3,99 +3,11 @@
 
 import sys
 
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QLabel, QAction, QPushButton, QVBoxLayout, QGroupBox, QMenu
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtCore import QSize, QRect
 
-class MainWindow(QtWidgets.QMainWindow):
+from ui.views.main_view import MainView
 
-    closeEventSignal = QtCore.pyqtSignal(QtGui.QCloseEvent)
-
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-        self.title = "RustDaVinci"
-        self.width = 240
-        self.height = 400
-        self._initUI()
-
-
-    def _initUI(self):
-        self.setWindowTitle(self.title)
-        self.setWindowIcon(QIcon("../images/RustDaVinci-icon.ico"))
-        self.resize(self.width, self.height)
-        self.setMinimumSize(QSize(self.width, self.height))
-
-        loadImageButton = QPushButton("               Load Image...             ", self)
-        loadImageButton.setGeometry(QRect(10, 10, self.width - 20, 45))
-        loadImageButton.setFlat(True)
-        loadImageButton.setIcon(QtGui.QIcon("../images/load_image_icon.png"))
-        loadImageButton.setIconSize(QtCore.QSize(self.width - 20, 45))
-
-        loadMenu = QtWidgets.QMenu()
-        loadMenu.addAction("Load from File", self.loadImageFile)
-        loadMenu.addAction("Load from URL", self.loadImageURL)
-        loadMenu.addAction("Show preview", self.showPreview)
-        loadMenu.addAction("Clear image", self.clearCurrentImage)
-        loadImageButton.setMenu(loadMenu)
-
-        settingsButton = QPushButton("    Identify palette and frame...", self)
-        settingsButton.setGeometry(QRect(10, 65, self.width - 20, 45))
-        settingsButton.setFlat(True)
-        settingsButton.setIcon(QtGui.QIcon("../images/select_area_icon.png"))
-        settingsButton.setIconSize(QtCore.QSize(self.width - 20, 45))
-        settingsButton.clicked.connect(self.identifyAreas)
-
-        paintImageButton = QPushButton("               Paint Image...            ", self)
-        paintImageButton.setGeometry(QRect(10, 120, self.width - 20, 45))
-        paintImageButton.setFlat(True)
-        paintImageButton.setEnabled(False)
-        paintImageButton.setIcon(QtGui.QIcon("../images/paint_image_icon.png"))
-        paintImageButton.setIconSize(QtCore.QSize(self.width - 20, 45))
-        paintImageButton.clicked.connect(self.startPainting)
-
-        settingsButton = QPushButton("                 Settings                    ", self)
-        settingsButton.setGeometry(QRect(10, 175, self.width - 20, 45))
-        settingsButton.setFlat(True)
-        settingsButton.setIcon(QtGui.QIcon("../images/settings_icon.png"))
-        settingsButton.setIconSize(QtCore.QSize(self.width - 20, 45))
-        settingsButton.clicked.connect(self.settings)
-
-    def loadImageFile(self):
-        print("load image file...")
-
-    def loadImageURL(self):
-        print("load image URL...")
-
-    def showPreview(self):
-        print("Showing preview of dithered image...")
-
-    def clearCurrentImage(self):
-        print("Cleared the current Image...")
-
-    def identifyAreas(self):
-        print("Identify palette and frame...")
-
-    def startPainting(self):
-        print("Start painting")
-
-    def settings(self):
-        print("Settings")
-
-
-
-    def show(self):
-        super(MainWindow, self).show()
-
-
-    def hide(self):
-        super(MainWindow, self).hide()
-
-
-    def closeEvent(self, closeEvent):
-        super(MainWindow, self).closeEvent(closeEvent)
-        self.closeEventSignal.emit(closeEvent)
 
 def run():
 
@@ -106,14 +18,11 @@ def run():
     # Setup the application and start
     app = QtWidgets.QApplication(sys.argv)
 
-    print(QtCore.QSettings().value("singleton", 0))
-    exit()
+    #print(QtCore.QSettings().value("singleton", 0))
 
-
-    main = MainWindow()
+    main = MainView()
     main.show()
     sys.exit(app.exec_())
-
 
 
 
