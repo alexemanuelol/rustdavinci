1d6774d83307c85597c813b85efcbc9cdf055198
Alexander <Alexander.Emanuelsson94@gmail.com>
2019-09-29 18:59:41 +0200

    Replaced qt code layout with qt designer autogenerated .py file

---
+++ rustdavinci/ui/views/README.md
@@ -0,0 +1,10 @@
+# mainui.py generation
+
+Generate mainui.py by running the command:
+
+""" bash
+pyuic5 mainui.ui -o mainui.py
+
+"""
+
+Make sure that you replace "import icons_rc" with "import ui.resources.icons_rc" in the mainui.py file
--- rustdavinci/ui/views/main.py
+++ rustdavinci/ui/views/main.py
@@ -9,7 +9,7 @@ from PyQt5.QtCore import QSize, QRect
 
 from ui.settings.settings import Settings
 
-#from ui.views.mainui import Ui_MainUI
+from ui.views.mainui import Ui_MainUI
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -17,91 +17,60 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        self.title = "RustDaVinci"
-        self.width = 240
-        self.height = 400
-        self._initUI()
 
-        #self.ui = Ui_MainUI()
-        #self.ui.setupUi(self)
-        #self.settings = QtCore.QSettings()
+        self.ui = Ui_MainUI()
+        self.ui.setupUi(self)
+
+        self.settings = QtCore.QSettings()
+
+        self.connectAll()
 
 
-    def loadImageFile(self):
+
+    def connectAll(self):
+        """ Connect all the buttons """
+        loadMenu = QtWidgets.QMenu()
+        loadMenu.addAction("Load from File", self.loadImageFile_clicked)
+        loadMenu.addAction("Load from URL", self.loadImageURL_clicked)
+        loadMenu.addAction("Show preview", self.showPreview_clicked)
+        loadMenu.addAction("Clear image", self.clearCurrentImage_clicked)
+        self.ui.loadImagePushButton.setMenu(loadMenu)
+
+        self.ui.identifyAreasPushButton.clicked.connect(self.identifyAreas_clicked)
+        self.ui.paintImagePushButton.clicked.connect(self.startPainting_clicked)
+        self.ui.settingsPushButton.clicked.connect(self.settings_clicked)
+
+
+    def loadImageFile_clicked(self):
         print("load image file...")
 
-    def loadImageURL(self):
+    def loadImageURL_clicked(self):
         print("load image URL...")
 
-    def showPreview(self):
+    def showPreview_clicked(self):
         print("Showing preview of dithered image...")
 
-    def clearCurrentImage(self):
+    def clearCurrentImage_clicked(self):
         print("Cleared the current Image...")
 
-    def identifyAreas(self):
+    def identifyAreas_clicked(self):
         print("Identify palette and frame...")
 
-    def startPainting(self):
+    def startPainting_clicked(self):
         print("Start painting")
 
-    def settings(self):
+    def settings_clicked(self):
         settings = Settings(self)
         settings.setModal(True)
         settings.show()
         print("Settings")
 
 
-
     def show(self):
         super(MainWindow, self).show()
 
-
     def hide(self):
         super(MainWindow, self).hide()
-
-
-    def _initUI(self):
-        self.setWindowTitle(self.title)
-        self.setWindowIcon(QIcon("../images/RustDaVinci-icon.ico"))
-        self.resize(self.width, self.height)
-        self.setMinimumSize(QSize(self.width, self.height))
-
-        loadImagePushButton = QPushButton("               Load Image...             ", self)
-        loadImagePushButton.setGeometry(QRect(10, 10, self.width - 20, 45))
-        loadImagePushButton.setFlat(True)
-        loadImagePushButton.setIcon(QtGui.QIcon("../images/load_image_icon.png"))
-        loadImagePushButton.setIconSize(QtCore.QSize(self.width - 20, 45))
-
-        loadMenu = QtWidgets.QMenu()
-        loadMenu.addAction("Load from File", self.loadImageFile)
-        loadMenu.addAction("Load from URL", self.loadImageURL)
-        loadMenu.addAction("Show preview", self.showPreview)
-        loadMenu.addAction("Clear image", self.clearCurrentImage)
-        loadImagePushButton.setMenu(loadMenu)
-
-        identifyAreasPushButton = QPushButton("    Identify palette and frame...", self)
-        identifyAreasPushButton.setGeometry(QRect(10, 65, self.width - 20, 45))
-        identifyAreasPushButton.setFlat(True)
-        identifyAreasPushButton.setIcon(QtGui.QIcon("../images/select_area_icon.png"))
-        identifyAreasPushButton.setIconSize(QtCore.QSize(self.width - 20, 45))
-        identifyAreasPushButton.clicked.connect(self.identifyAreas)
-
-        paintImagePushButton = QPushButton("               Paint Image...            ", self)
-        paintImagePushButton.setGeometry(QRect(10, 120, self.width - 20, 45))
-        paintImagePushButton.setFlat(True)
-        paintImagePushButton.setEnabled(False)
-        paintImagePushButton.setIcon(QtGui.QIcon("../images/paint_image_icon.png"))
-        paintImagePushButton.setIconSize(QtCore.QSize(self.width - 20, 45))
-        paintImagePushButton.clicked.connect(self.startPainting)
-
-        settingsPushButton = QPushButton("                 Settings                    ", self)
-        settingsPushButton.setGeometry(QRect(10, 175, self.width - 20, 45))
-        settingsPushButton.setFlat(True)
-        settingsPushButton.setIcon(QtGui.QIcon("../images/settings_icon.png"))
-        settingsPushButton.setIconSize(QtCore.QSize(self.width - 20, 45))
-        settingsPushButton.clicked.connect(self.settings)
-
 
     def closeEvent(self, closeEvent):
         super(MainWindow, self).closeEvent(closeEvent)
