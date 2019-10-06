5fc8e09b9bd6333501ff08d8302a44395897963e
Alexemanuelol <Alexander.Emanuelsson94@gmail.com>
2019-10-06 17:49:22 +0200

    Transfer from laptop, add quantized function and convert function

--- rustdavinci/lib/rustDaVinci.py
+++ rustdavinci/lib/rustDaVinci.py
@@ -10,6 +10,7 @@ import cv2
 import numpy
 
 from lib.captureArea import capture_area
+from lib.rustPaletteData import palette_80
 
 class rustDaVinci():
 
@@ -33,8 +34,28 @@ class rustDaVinci():
         self.canvas_located = False
         self.ctrl_located = False
 
+        self.ctrl_remove = 0
+        self.ctrl_update = 0
+        self.ctrl_size = []
+        self.ctrl_brush = []
+        self.ctrl_opacity = []
+        self.ctrl_color = []
+
         self.original_img = None
         self.quantized_img = None
+        self.pixel_arr = None
+
+        self.img_colors = []
+        self.tot_pixels = 0
+        self.pixels = 0
+        self.lines = 0
+
+        self.est_time_lines = 0
+        self.est_time_click = 0
+
+        self.is_paused = False
+        self.is_skip_color = False
+        self.is_exit = False
 
 
     def load_image_from_file(self):
@@ -185,6 +206,7 @@ class rustDaVinci():
                 self.ctrl_y = int(self.settings.value("ctrl_y", "0"))
                 self.ctrl_w = int(self.settings.value("ctrl_w", "0"))
                 self.ctrl_h = int(self.settings.value("ctrl_h", "0"))
+                self.update_paint_button()
                 return
 
 
@@ -263,7 +285,141 @@ class rustDaVinci():
         self.update_paint_button()
 
 
+    def calc_ctrl_tools_pos(self):
+        """ This function calculates the positioning of the different controls in the painting control area.
+        The brush size, type and opacity along with all the different colors.
+        Updates:    self.ctrl_remove
+                    self.ctrl_update
+                    self.ctrl_size
+                    self.ctrl_brush
+                    self.ctrl_opacity
+                    self.ctrl_color
+        """
+        # Calculate the distance between two items on a row of six items (Size)
+        first_x_coord_of_six_v1 = self.ctrl_x + (self.ctrl_w/6.5454)
+        second_x_coord_of_six_v1 = self.ctrl_x + (self.ctrl_w/3.4285)
+        dist_btwn_x_coords_of_six_v1 = second_x_coord_of_six_v1 - first_x_coord_of_six_v1
+    
+        # Calculate the distance between two items on a row of six items (Opacity)
+        first_x_coord_of_six_v2 = self.ctrl_x + (self.ctrl_w/7.5789)
+        second_x_coord_of_six_v2 = self.ctrl_x + (self.ctrl_w/3.5555)
+        dist_btwn_x_coords_of_six_v2 = second_x_coord_of_six_v2 - first_x_coord_of_six_v2
+    
+        # Calculate the distance between two items on a row of four items (Colors width)
+        first_x_coord_of_four = self.ctrl_x + (self.ctrl_w/6)
+        second_x_coord_of_four = self.ctrl_x + (self.ctrl_w/2.5714)
+        dist_btwn_x_coords_of_four = second_x_coord_of_four - first_x_coord_of_four
+    
+        # Calculate the distance between two items on a column of eight items (Colors height)
+        first_y_coord_of_eight = self.ctrl_y + (self.ctrl_h/2.3220)
+        second_y_coord_of_eight = self.ctrl_y + (self.ctrl_h/1.9855)
+        dist_btwn_y_coords_of_eight = second_y_coord_of_eight - first_y_coord_of_eight
+    
+        # Set the point location of the remove & update buttons
+        self.ctrl_remove = ((self.ctrl_x + (self.ctrl_w/2.7692)), (self.ctrl_y + (self.ctrl_h/19.5714)))
+        self.ctrl_update = ((self.ctrl_x + (self.ctrl_w/1.5652)), (self.ctrl_y + (self.ctrl_h/19.5714)))
+    
+    
+        for size in range(6):
+            self.ctrl_size.append((  first_x_coord_of_six_v1 + 
+                                     (size * dist_btwn_x_coords_of_six_v1), 
+                                     (self.ctrl_y + (self.ctrl_h/6.9661))))
+    
+        for brush in range(4):
+            self.ctrl_brush.append(( first_x_coord_of_four + 
+                                     (brush * dist_btwn_x_coords_of_four), 
+                                     (self.ctrl_y + (self.ctrl_h/4.2371))))
+    
+        for opacity in range(6):
+            self.ctrl_opacity.append((   first_x_coord_of_six_v2 + 
+                                         (opacity * dist_btwn_x_coords_of_six_v2), 
+                                         (self.ctrl_y + (self.ctrl_h/3.0332))))
+    
+        for row in range(8):
+            for column in range(4):
+                if (row == 0 or row == 4) and column == 3: continue
+                if (row == 1 or row == 5) and (column == 2 or column == 3): continue
+                if row == 2 and column == 0: continue
+                if row == 3 and (column == 0 or column == 1): continue
+                if row == 6 and column == 2: continue
+                if row == 7 and (column == 1 or column == 2): continue
+                self.ctrl_color.append(  (first_x_coord_of_four + (column * dist_btwn_x_coords_of_four),
+                                         (first_y_coord_of_eight + (row * dist_btwn_y_coords_of_eight))))
+    
+        # TODO: Experiment with positioning of new (hidden) colors. Append individual colors to the control_color list
 
+
+    def convert_img(self):
+        """ Convert the image to fit the canvas and quantize the image.
+        Updates:    quantized_img,
+                    x_correction,
+                    y_correction
+        Returns:    False, if the image type is invalid.
+        """
+        org_img_w = self.original_img.size[0]
+        org_img_h = self.original_img.size[1]
+            
+        wpercent = (self.canvas_w / float(org_img_w))
+        hpercent = (self.canvas_h / float(org_img_h))
+    
+        hsize = int((float(org_img_h) * float(wpercent)))
+        wsize = int((float(org_img_w) * float(hpercent)))
+    
+        x_correction = 0
+        y_correction = 0
+    
+        if hsize <= self.canvas_h: 
+            resized_img = self.original_img.resize((self.canvas_w, hsize), Image.ANTIALIAS)
+            y_correction = int((self.canvas_h - hsize)/2)
+        elif wsize <= self.canvas_w: 
+            resized_img = self.original_img.resize((wsize, self.canvas_h), Image.ANTIALIAS)
+            x_correction = int((self.canvas_w - wsize)/2)
+        else: 
+            resized_img = self.original_img.resize((self.canvas_w, self.canvas_h), Image.ANTIALIAS)
+    
+        # Select the palette to be used
+        palette_data = Image.new("P", (1, 1))
+        palette_data.putpalette(rustPaletteData.palette_80)
+    
+        self.quantized_img = quantize_to_palette(resized_img, palette_data)
+        if self.quantized_img == False:
+            self.quantized_img = None
+            return False
+            
+        #self.pixel_arr = self.quantized_img.load()
+
+        self.canvas_x += x_correction
+        self.canvas_y += y_correction
+        self.canvas_w = self.quantized_img.size[0]
+        self.canvas_h = self.quantized_img.size[1]
+
+
+    def quantize_to_palette(self, original_img, palette):
+        """ Convert an RGB, RGBA or L mode image to use a given P image's palette.
+        Returns:    The quantized image
+        """
+        original_img.load()
+        palette.load()
+    
+        if original_img.mode == "RGBA":
+            print("\nWarning! This Image was RGBA, converting to RGB...\n")
+            original_img = original_img.convert("RGB")
+    
+        if palette.mode != "P":
+            raise ValueError("Bad mode for palette image")
+    
+        if original_img.mode != "RGB" and original_img.mode != "L":
+            raise ValueError("Only RGB or L mode images can be quantized to a palette")
+    
+    
+        quality = int(self.settings.value("painting_quality", 1))
+        if quality == 0:
+            im = original_img.im.convert("P", 0, palette.im)
+        elif quality == 1:
+            im = original_img.im.convert("P", 1, palette.im) # Dithering
+    
+        try: return original_img._new(im)
+        except AttributeError: return original_img._makeself(im)
 
 
 
